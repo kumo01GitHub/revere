22331afca54a96f60b525ec6223a3cf6bfebc172
kumo01GitHub <kumo01@outlook.com>
2026-04-04 17:31:21 +0900

    fix: rename SPM package target to swift_log_transport to match Flutter module name

--- packages/swift_log_transport/Package.swift
+++ packages/swift_log_transport/Package.swift
@@ -2,21 +2,21 @@
 import PackageDescription
 
 let package = Package(
-    name: "SwiftLogTransportPlugin",
+    name: "swift_log_transport",
     platforms: [
         .iOS(.v11), .macOS(.v10_13)
     ],
     products: [
         .library(
-            name: "SwiftLogTransportPlugin",
-            targets: ["SwiftLogTransportPlugin"]),
+            name: "swift_log_transport",
+            targets: ["swift_log_transport"]),
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-log.git", from: "1.4.0")
     ],
     targets: [
         .target(
-            name: "SwiftLogTransportPlugin",
+            name: "swift_log_transport",
             dependencies: [
                 .product(name: "Logging", package: "swift-log")
             ],
