c5e72b00d6eb0a03f92720a6c388ea55ae62856c
kumo01GitHub <kumo01@outlook.com>
2026-04-04 17:02:48 +0900

    ci: fix swift_log_transport test setup and untrack Package.resolved

--- packages/swift_log_transport/test/swift_log_transport_test.dart
+++ packages/swift_log_transport/test/swift_log_transport_test.dart
@@ -1,8 +1,25 @@
+import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:revere/core.dart';
 import 'package:swift_log_transport/swift_log_transport.dart';
 
 void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  const MethodChannel channel = MethodChannel('swift_log_transport');
+
+  setUp(() {
+    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
+        .setMockMethodCallHandler(channel, (MethodCall methodCall) async {
+      return null;
+    });
+  });
+
+  tearDown(() {
+    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
+        .setMockMethodCallHandler(channel, null);
+  });
+
   test('SwiftLogTransport can be instantiated with defaults', () {
     final transport = SwiftLogTransport();
     expect(transport, isA<SwiftLogTransport>());
